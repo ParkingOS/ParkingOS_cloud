caed646fbaf12f6669778ae317ed35a03119f487
laoyao <you@example.com>
2017-06-29 10:53:38 +0800

    edit config

--- parkingos_cloud_api/src/pay/AlipayConfig.java
+++ parkingos_cloud_api/src/pay/AlipayConfig.java
@@ -25,35 +25,24 @@ public class AlipayConfig {
 	
 	//↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 	// 合作身份者ID，以2088开头由16位纯数字组成的字符串
-	public static String partner =PayConfigDefind.getValue("PARTNER");// "2088411488582814";
+	public static String partner =PayConfigDefind.getValue("PARTNER");
 	
 	 /** 服务窗appId  */
     //TODO !!!! 注：该appId必须设为开发者自己的服务窗id  这里只是个测试id
-    public static final String APP_ID =PayConfigDefind.getValue("APP_ID");// "2016080801718601";
+    public static final String APP_ID =PayConfigDefind.getValue("APP_ID")
     
 	
 	/**支付宝网关*/
-    public static final String ALIPAY_GATEWAY    = PayConfigDefind.getValue("ALI_PAY_GATEWAY");//"https://openapi.alipay.com/gateway.do";
-	// 商户的私钥
-	//public static String private_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDL/H9RKsETeGceVjt5Vc3ulilnUEFpjlgJ8pxhyJbEdfOFBJZdVePOfadwgnPW+68GWjq7o9ipz01Cf1akPsT3+0PKOtTxZZX8yYENSozcPhiUydWVEwxH+ON13qGuonkm2Y1nEerjbUOX3wxY8etoI91CEyI8UwqUXD7plIAcBwIDAQAB";
-	//public static String private_key = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBANSd+DwFRq5hjWcxzMLNJXw1nkRG+c/aXbBse4JEX7UWhYW73bEwxlJ5gpbusxm1iM0wgGGfEBRL0tHCvyFfy5Ymw3pt+V/2kwgH77aSUHJN4HzmpJEtxXTqves2RfTG6ISDJEkzKSCB8jDTNl8YE0dNCu6WVGjx9O9lCRiL3NvxAgMBAAECgYEApwrJOVan1l88OgQtLCsCtVhm1JuyrrOQAgjo7EqNqvdbxdr4bLq2RZvDlpNI0P0H0rW1V30ho+CCbFyFz1G03v9AHtVwb0wVFfnb11C+t6GcR4YQmIWYgI83+fDNUlecnOhZcTcGJNlHm5FP1MbPBbs1UrknrIb4smyVHZUJx/UCQQDuIS06aNZSB4WkaM6WL77FqDGxzwHA2M8bWwt5xx7a9VYKQjbgtjrlLI3wMSdOiPvZ7D/iLVLW1HjZlb8Y+4jTAkEA5JKo0QZDtHaTwtCmKdl4spOF7Z7e+wCYJgtd+tZWBNrFj0AF/9GEuiYfVvgKtqcf0d6ovrYKgNOerUMUvutNqwJBAN5hx04b58KVWb4PTpY9ImiOSVJnIpkJIGjInq/sP6l1ohNUgNFb/SZWdHtSPAYAsUpzcbl0YD9WD0ILsXnQNZcCQAP/NIiDLYfoUukjMOSmct/ciIkBMDD/b6mqpTgPq21mTfIVUoWIqmN+6ylgKP/MqFfJJvY+xKxABxRMif5UqwUCQHFXPdcV4r7S6lR2c9233+x+/SgmQtJfa5x6fmkCKT61AUl1hAGdi1pa4IfNwdXRcVmHocwZK4/rQ8sEJIX0NNk=";
-	public static String private_key =PayConfigDefind.getValue("PRIVATE_KEY");// "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMaRQH6IIotqQkMu8WJlYI0sCFWccbCLpixrUEdppJRYpREAF3Fb6D4MX9t3Ng/b78Gfx5hN/+uJq8IfjoaxUuuVILKl4DdNfs7L3oA9B77gXkjXWHMhp8qkCkIBVzpWFI1M4uNwifmjsG7qnWw3c0aKYOhONCvJ8vOF0vrUOlt9AgMBAAECgYAkIk6QOAnBQQbp3uMCOtyFFBw4KA3wSJlmv+iN9kWC0VbIbzHU6RqsH+hZsane3PEWVRMnPhpyLE1bOA7hp+cksWrxWMY4eCAoXJL0YAuh53qTGXq3E6oQAX3lRQ+dCcm4zRMODJ2zPR1RMceuM+5phU6QyjbNW2o5wTROYFoy6QJBAPJbtGpnYdDmf0+StynIPUaEh9ogZynRQGCihNDTlsqKDRM4A8OwJ/dHhczL3SWRNEJoikjxpGROJSjd3LmlkQsCQQDRvol3BvQA5Wej5LrM7R84pMObVtwMa7DeQs05rr1nE3B+ykaAxANT35D/Z4TGT4PtNhm+LJmjY1qA48hFEiqXAkAzQiwNWSI6EZYZmVk98AjtsjgdbT8EfCpWhej7VdUNr1cGmpFJQSeiyDDHWNLEEEryRLQCq4Duagy38PyvExJzAkEAiXDi4WVkBSZOaL5cjiaf+90z9JnRbi4vYyXBF5hisqWxZNQlqQFI8PAiMsrh3ZWDbLCz48OUFdXoG2en92L1FwJBALHUc+agJaSysvth4X7RLCnj9L6PplIqv0ggnHhLV8K1KlnQ6FlRAaa5EILdln+DyyD8g+LNSO6aXvrgHBo9naU=";
-
-	// 支付宝的公钥，无需修改该值
-	public static String ali_public_key  =PayConfigDefind.getValue("ALI_PUBLIC_KEY");// "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB";
-
-	//public static String ali_public_key  = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB";
-	//海信支付宝公钥
-	 
+    public static final String ALIPAY_GATEWAY    = PayConfigDefind.getValue("ALI_PAY_GATEWAY");
+		// 支付宝的公钥，无需修改该值
+	public static String ali_public_key  =PayConfigDefind.getValue("ALI_PUBLIC_KEY");
 	//扫码公钥
-	public static String ALIPUBLICKEY4QR  =PayConfigDefind.getValue("ALI_PUBLICKEY_4QR");// "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI6d306Q8fIfCOaTXyiUeJHkrIvYISRcc73s3vF1ZT7XN8RNPwJxo8pWaJMmvyTn9N4HQ632qJBVHf8sxHi/fEsraprwCtzvzQETrNRwVxLO5jVmRGi60j8Ue1efIlzPXV9je9mkjzOmdssymZkh2QhUrCmZYI/FCEa3/cNMW0QIDAQAB";
-										  //MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI6d306Q8fIfCOaTXyiUeJHkrIvYISRcc73s3vF1ZT7XN8RNPwJxo8pWaJMmvyTn9N4HQ632qJBVHf8sxHi/fEsraprwCtzvzQETrNRwVxLO5jVmRGi60j8Ue1efIlzPXV9je9mkjzOmdssymZkh2QhUrCmZYI/FCEa3/cNMW0QIDAQAB
-	//↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+	public static String ALIPUBLICKEY4QR  =PayConfigDefind.getValue("ALI_PUBLICKEY_4QR");
 	
 	// 字符编码格式 目前支持 gbk 或 utf-8
-	public static String input_charset =PayConfigDefind.getValue("CHARSET");// "utf-8";
+	public static String input_charset =PayConfigDefind.getValue("CHARSET");
 	
 	// 签名方式 不需修改
-	public static String sign_type =PayConfigDefind.getValue("SIGN_TYPE");// "RSA";
+	public static String sign_type =PayConfigDefind.getValue("SIGN_TYPE");
 
 }
--- parkingos_cloud_api/src/pay/Constants.java
+++ parkingos_cloud_api/src/pay/Constants.java
@@ -3,73 +3,62 @@ package pay;
 public class Constants {
 	// ----------------------微信支付Key--------------------------------------
 	//----------------------停车宝--------------------------------------
-	public static final String WXPAY_APPID = PayConfigDefind.getValue("WXPAY_APPID");//"wx73454d7f61f862a5";
-	public static final String WXPAY_PARTNERID = PayConfigDefind.getValue("WXPAY_PARTNERID");//"1220886701";
-	public static final String WXPAY_APPSECRET = PayConfigDefind.getValue("WXPAY_APPSECRET");//"b3e563822a872e5a37eb692a856ed4ba";
-	public static final String WXPAY_PARTNERKEY = PayConfigDefind.getValue("WXPAY_PARTNERKEY");//"d7b993008827a203659ca008372382fe";
-	public static final String WXPAY_APPKEY = PayConfigDefind.getValue("WXPAY_APPKEY");//"kkyTJVjVnxOYDh7hhRQinfxWyIYEDBSRSm72VZWvKyxwpczPjODQpqRHdy3JIHuVJKjlJS0UwaINGBx5HziqfERh0W8tQ3v0aXmheFLIzscSdBA0vrkjkLeoyOffA2PW";
+	public static final String WXPAY_APPID = PayConfigDefind.getValue("WXPAY_APPID");
+	public static final String WXPAY_PARTNERID = PayConfigDefind.getValue("WXPAY_PARTNERID");
+	public static final String WXPAY_APPSECRET = PayConfigDefind.getValue("WXPAY_APPSECRET");
+	public static final String WXPAY_PARTNERKEY = PayConfigDefind.getValue("WXPAY_PARTNERKEY");
+	public static final String WXPAY_APPKEY = PayConfigDefind.getValue("WXPAY_APPKEY");
 
 	public static final String WXPAY_GETTOKEN_URL = PayConfigDefind.getValue("WXPAY_GETTOKEN_URL")+"&appid="+WXPAY_APPID+"&secret=" + WXPAY_APPSECRET;//"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
 			//+ WXPAY_APPID + "&secret=" + WXPAY_APPSECRET;
 	public static final String WXPAY_GETPREPAYID_URL =PayConfigDefind.getValue("WXPAY_GETPREPAYID_URL");// "https://api.weixin.qq.com/pay/genprepay";
 	
-	//----------------------微信公众号-------------------------------------
-	//----------------------测试-----------------------------------------
-//	public static String WXPUBLIC_APPID = "wxa4c8632273026181";
-//	public static String WXPUBLIC_SECRET = "15be9a3be73cbefe633627c83b29b77a";
-//	public static  String WXPUBLIC_REDIRECTURL = "wang151068941.oicp.net"; 
-	
-//	public static  String WXPUBLIC_REDIRECTURL = "wang151068941.oicp.net"; 
 
-///	==============yao test
-//	public static  String WXPUBLIC_APPID =  "wx7cc6fd2efd5144e3";
-//	public static  String WXPUBLIC_SECRET = "1d497fd54dcfde7afe19abdac817a3d5";
-//	public static  String WXPUBLIC_REDIRECTURL = "yxiudongyeahnet.vicp.cc"; 
 
-	public static  String WXPUBLIC_APPID =PayConfigDefind.getValue("WXPUBLIC_APPID");// "wx08c66cac888faa2a";
-	public static  String WXPUBLIC_SECRET = PayConfigDefind.getValue("WXPUBLIC_SECRET");//"41e81defb9b0a7494632a6af04209a82";
-	public static  String WXPUBLIC_REDIRECTURL =PayConfigDefind.getValue("WXPUBLIC_REDIRECTURL");// "s.tingchebao.com";
+	public static  String WXPUBLIC_APPID =PayConfigDefind.getValue("WXPUBLIC_APPID");
+	public static  String WXPUBLIC_SECRET = PayConfigDefind.getValue("WXPUBLIC_SECRET");
+	public static  String WXPUBLIC_REDIRECTURL =PayConfigDefind.getValue("WXPUBLIC_REDIRECTURL");
 	
 	public static  String LOCAL_NAME =PayConfigDefind.getValue("LOCAL_NAME");// "zld";
 	
 //	public static  String WXPUBLIC_REDIRECTURL = "192.168.199.239";
 //	public static  String LOCAL_NAME = "zldi";
 	
-	public static  String WXPUBLIC_S_DOMAIN = PayConfigDefind.getValue("WXPUBLIC_S_DOMAIN");//"s.tingchebao.com";
+	public static  String WXPUBLIC_S_DOMAIN = PayConfigDefind.getValue("WXPUBLIC_S_DOMAIN");
 	
-	public static final String WXPUBLIC_MCH_ID =PayConfigDefind.getValue("WXPUBLIC_MCH_ID");// "1229325602";
-	public static final String WXPUBLIC_APPKEY =PayConfigDefind.getValue("WXPUBLIC_APPKEY");// "zhenlaidianbeijingkejiyouxiangon";
+	public static final String WXPUBLIC_MCH_ID =PayConfigDefind.getValue("WXPUBLIC_MCH_ID");
+	public static final String WXPUBLIC_APPKEY =PayConfigDefind.getValue("WXPUBLIC_APPKEY");
 	//获取access_token
 	public static String WXPUBLIC_GETTOKEN_URL =PayConfigDefind.getValue("WXPUBLIC_GETTOKEN_URL")+"&appid="+ WXPUBLIC_APPID + "&secret=" + WXPUBLIC_SECRET;//"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
 			//+ WXPUBLIC_APPID + "&secret=" + WXPUBLIC_SECRET;
 	//统一支付接口
-	public static String WXPUBLIC_UNIFIEDORDER = PayConfigDefind.getValue("WXPUBLIC_UNIFIEDORDER");//"https://api.mch.weixin.qq.com/pay/unifiedorder";
+	public static String WXPUBLIC_UNIFIEDORDER = PayConfigDefind.getValue("WXPUBLIC_UNIFIEDORDER");
 	//通知地址
-	public static String WXPUBLIC_NOTIFY_URL =PayConfigDefind.getValue("WXPUBLIC_NOTIFY_URL");// "http://"+WXPUBLIC_S_DOMAIN+"/zld/wxphandle";
+	public static String WXPUBLIC_NOTIFY_URL =PayConfigDefind.getValue("WXPUBLIC_NOTIFY_URL");
 	
 	//退款地址
-	public static String WXPUBLIC_BACK_URL =PayConfigDefind.getValue("WXPUBLIC_BACK_URL");// "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";
+	public static String WXPUBLIC_BACK_URL =PayConfigDefind.getValue("WXPUBLIC_BACK_URL");
 	
 	//通知地址(测试)
 //	public static final String WXPUBLIC_NOTIFY_URL = "http://wang151068941.oicp.net/zld/wxphandle";
 	
-	public static String WXPUBLIC_SUCCESS_NOTIFYMSG_ID = "dhyfJiJAhe8iZE39HD2m5U-Qc1jeGa0h4-_ynhrlrgA";//订单支付成功
+	public static String WXPUBLIC_SUCCESS_NOTIFYMSG_ID = "dhyfJiJAhe8iZE39HD2m5U--_ynhrlrgA";//订单支付成功
 	
-	public static String WXPUBLIC_FAIL_NOTIFYMSG_ID = "DoUGP9qSXSeIV_Y0mY0tHdpbx0qXvTUxmsDWP19a-H0";//订单支付失败
+	public static String WXPUBLIC_FAIL_NOTIFYMSG_ID = "-H0";//订单支付失败
 	
-	public static String WXPUBLIC_BONUS_NOTIFYMSG_ID = "DFzXEfyMtIO2GmAreD7pDd02t2KEBAGpAOpF4aOB4E4";//打赏通知
+	public static String WXPUBLIC_BONUS_NOTIFYMSG_ID = "";//打赏通知
 	//未付款订单通知
-	public static String WXPUBLIC_ORDER_NOTIFYMSG_ID = "gCTGzyprc1N1dhIcRYBlTIKOMTExo0QPBKSW6NF480o";
+	public static String WXPUBLIC_ORDER_NOTIFYMSG_ID = "";
 	
-	public static String WXPUBLIC_BACK_NOTIFYMSG_ID = "2Th_VLboH7OotEa4csiXw0_-ejXVs31B0lMn42ftpN8";//退款
+	public static String WXPUBLIC_BACK_NOTIFYMSG_ID = "-ejXVs31B0lMn42ftpN8";//退款
 	
-	public static String WXPUBLIC_TICKET_ID = "9bgsmMKaKKfGpNbQoFFuyvqfv2Pd504GZamIcBm6AAU";//获得代金券通知
+	public static String WXPUBLIC_TICKET_ID = "";//获得代金券通知
 	
-	public static String WXPUBLIC_AUDITRESULT_ID = "DP2IHNX-OH7NyR54mIA0VPa77h8BaJhqtp1XiCJ4FSc";//审核结果通知
+	public static String WXPUBLIC_AUDITRESULT_ID = "DP2IHNX-";//审核结果通知
 	
-	public static String WXPUBLIC_FLYGMAMEMESG_ID = "2Fdm4xKSfE8jI_jmYcFQKRq5iBn1HDn0_O16ksWAZBk";//名片交换通知，打灰机加好机友
+	public static String WXPUBLIC_FLYGMAMEMESG_ID = "";//名片交换通知，打灰机加好机友
 	
-	public static String WXPUBLIC_LEAVE_MESG_ID = "IS-0WgX_gcrxGHgYypAxRojx0jQl_nLBq4PBAst5GFE";//留言
+	public static String WXPUBLIC_LEAVE_MESG_ID = "IS-";//留言
 	
 	public static class ShowMsgActivity {
 		public static final String STitle = "showmsg_title";
